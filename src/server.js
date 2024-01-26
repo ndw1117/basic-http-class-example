ae4ae091ff0ef03907c2961337af47bc22ca2646
ndw1117 <ndw1117@g.rit.edu>
2024-01-26 14:47:02 -0500

    Did in class demo

--- src/server.js
+++ src/server.js
@@ -1,3 +1,24 @@
-const myVar = 5;
+const http = require('http');
+const fs = require('fs');
 
-console.log(myVar);
+const port = process.env.port || process.env.NODE_PORT || 3000;
+
+const index = fs.readFileSync(`${__dirname}/../client/client.html`);
+const css = fs.readFileSync(`${__dirname}/../client/style.css`);
+
+const onRequest = (request, response) => {
+    if (request.url === '/style.css') {
+        response.writeHead(200, {'Content-Type': 'text/css'});
+        response.write(css);
+        response.end();
+    }
+    else {
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.write(index);
+        response.end();
+    }
+};
+
+http.createServer(onRequest).listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
